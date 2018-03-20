c281ccbbfd81c6c57a805c90ab03a0ef76fe9fdb
Unknown <Kwei3@sheffield.ac.uk>
2018-03-20 22:35:38 +0000

    1

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def get_feedback():
 
 # Passing in "debug=True" as an (keyword) argument to app.run(...) will make your Flask powered
 # server/application refreshes automatically when you make any changes to this file.
--"""
- -This piece of logic checks whether you are running the app locally or on Heroku
- -(make an account at https://www.heroku.com/ before the deployment session!). When
- -running the app on Heroku, the PORT environment/config variable is pre-populated by
- -Heroku to tell our app the correct port to run on.
- -"""
- -if "PORT" in os.environ:
- -    app.run(host="0.0.0.0", port=int(os.environ["PORT"]))
- -else:
- -    app.run(debug=True)
+"""
+This piece of logic checks whether you are running the app locally or on Heroku
+(make an account at https://www.heroku.com/ before the deployment session!). When
+running the app on Heroku, the PORT environment/config variable is pre-populated by
+Heroku to tell our app the correct port to run on.
+"""
+if "PORT" in os.environ:
+    app.run(host="0.0.0.0", port=int(os.environ["PORT"]))
+else:
+    app.run(debug=True)
